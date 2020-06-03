3dfe1208e423580deed62fb655b77edd410e1f67
IlyaIvanchikov <phantomonster1@mail.ru>
2020-06-03 17:14:37 +0300

    init: add ts,tslint,prettier,scss

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p className={classes.test}>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
