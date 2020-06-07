73f46f0aa748cb2bb4f6872b171712f4d276d5dd
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-06-07 19:48:01 +0200

    fix: delete style

--- src/container/header/header.tsx
+++ src/container/header/header.tsx
@@ -3,7 +3,7 @@ import classes from './header.module.scss';
 import { Row, Col, Container } from 'react-bootstrap';
 import logo from '../../resources/images/header/logo.svg';
 import arrow from '../../resources/images/header/arrow.svg';
-import brain from '../../resources/images/header/dumbbells.svg';
+import dumbbells from '../../resources/images/header/dumbbells.svg';
 
 const Header: React.FC = () => (
   <Container fluid={true} className={classes.container}>
@@ -16,11 +16,11 @@ const Header: React.FC = () => (
         </div>
       </Col>
       <Col className="flex-row justify-content-center align-items-center d-none d-md-flex">
-        <img src={arrow} alt="arrow" className={classes.arrow} />
+        <img src={arrow} alt="arrow" />
         <h1>Форсаж</h1>
       </Col>
       <Col className="d-flex flex-row justify-content-sm-end align-items-center justify-content-center">
-        <img src={brain} alt="brain" className={classes.brain} />
+        <img src={dumbbells} alt="brain" />
         <a href="/">К тренажерам</a>
       </Col>
     </Row>
