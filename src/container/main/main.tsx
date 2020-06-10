1f698f0c608415348cf439927decd4027d0b7223
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-06-10 19:32:48 +0200

    feat: add 6 parameters

--- src/container/main/main.tsx
+++ src/container/main/main.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import classes from './main.module.scss';
 // import ModalComponent from '../modal-component/modal-component';
-// import { Row, Col, Button } from 'react-bootstrap';
-
+import BlockParameters from './../../components/blockParameters/blockParameters';
 const Main: React.FC = () => {
   // const [show, setShow] = useState(false);
 
   return (
     <main className={classes.main}>
-      <p>Hello</p>
-      {/* <Row>
-        <Col>
-          <Button variant="primary" onClick={() => setShow(true)}>
-            Launch static backdrop modal
-          </Button>
-          <ModalComponent showModal={show} />
-        </Col>
-      </Row> */}
+      <BlockParameters />
+      {/* <Container className={classes.container}>
+        <h2 className="text-center">Выберите параметры:</h2>
+        <Row>
+          <Col>Количество игроков:</Col>
+          <Col>
+            <button className={classes}>1 игрок</button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>Скорость:</Col>
+          <Col>Test2</Col>
+        </Row>
+        <Row>
+          <Col>Количество знаков:</Col>
+          <Col>Test2</Col>
+        </Row>
+        <Row>
+          <Col>Количество примеров:</Col>
+          <Col>Test2</Col>
+        </Row>
+        <Row>
+          <Col>Количество цифр в примере</Col>
+          <Col>Test2</Col>
+        </Row>
+        <Row>
+          <Col>Законы</Col>
+          <Col>Test2</Col>
+        </Row>
+        <Row>
+          <Col>Дополнительные настройки</Col>
+          <Col>Test2</Col>
+        </Row>
+      </Container> */}
     </main>
   );
 };
