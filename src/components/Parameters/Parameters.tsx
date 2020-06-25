d188535792de7c64d9235d921880e1cd72311d45
Ilya Ivanchikov <phantomonster1@mail.ru>
2020-06-25 12:17:04 +0200

    feat: add laws module

--- src/components/Parameters/Parameters.tsx
+++ src/components/Parameters/Parameters.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './parameters.module.scss';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import Dropdown from './dropdown/dropdown';
@@ -7,18 +7,41 @@ import FormRange from './formRange/formRange';
 import ModalComponent from './modal/modal';
 import { Players } from './modal/components/players/players';
 import { Signs } from './modal/components/signs/signs';
+import { Laws } from './modal/components/laws/laws';
+import Loader from './../loader/loader';
 
 interface EventHandlerProps {
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
 const Parameters = ({ handleSubmit }: EventHandlerProps) => {
+  const [isLoading, setLoading] = useState<boolean>(false);
   const [showPlayers, setShowPlayers] = useState<boolean>(false);
   const [showSigns, setShowSigns] = useState<boolean>(false);
-  const handleModalPlayersClick = () => setShowPlayers(true);
+  const [showLaws, setShowLaws] = useState<boolean>(false);
+
+  const handleModalPlayersClick = () => {
+    setShowPlayers(true);
+    setLoading(true);
+  };
   const handleCloseModalPlayersClick = () => setShowPlayers(false);
   const handleModalSignsClick = () => setShowSigns(true);
   const handleCloseModalSignsClick = () => setShowSigns(false);
+
+  const handleModalLawsClick = () => setShowLaws(true);
+  const handleCloseModalLawsClick = () => setShowLaws(false);
+
+  const loaderTime = () => {
+    return new Promise<string>((resolve: any) => setTimeout(resolve, 2000));
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      loaderTime().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
 
   return (
     <Form className={classes.form} onSubmit={handleSubmit}>
@@ -33,7 +56,7 @@ const Parameters = ({ handleSubmit }: EventHandlerProps) => {
         title="Количество игроков"
       >
         {' '}
-        <Players />
+        {isLoading ? <Loader /> : <Players />}
       </ModalComponent>
       <FormRange title="Скорость:" />
       <FormButton
@@ -50,7 +73,15 @@ const Parameters = ({ handleSubmit }: EventHandlerProps) => {
       </ModalComponent>
       <FormRange title="Количество примеров:" />
       <FormRange title="Количество цифр в примере:" />
-      {/* <FormButton title="Законы:" /> */}
+      <FormButton title="Законы:" handleModalClick={handleModalLawsClick} />
+      <ModalComponent
+        showModal={showLaws}
+        handleCloseModalClick={handleCloseModalLawsClick}
+        title="Законы"
+      >
+        {' '}
+        <Laws />
+      </ModalComponent>
       <Form.Group as={Row} controlId="formPlaintextButton" className="mb-1">
         <Form.Label column={true} sm="7">
           <Dropdown />
